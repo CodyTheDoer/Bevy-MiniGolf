0488890af29b153248e50dc3a54bcb29daf76280
CodyTheDoer <me@codythedoer.com>
2024-11-12 15:20:58 -0500

    Woring on bonk interface logic. Drag to determine power logic is started.

--- src/main.rs
+++ src/main.rs
@@ -156,7 +156,9 @@ fn main() {
         // .add_systems(Update, bonk_gizmo.run_if(input_just_released(KeyCode::KeyB)))
         .add_systems(Update, bonk_gizmo.run_if(in_state(ArrowState::DrawingArrow)))
         .add_systems(Update, bonk.run_if(input_just_released(KeyCode::Space)))
-        .add_systems(Update, collision_events_listener);
+        .add_systems(Update, collision_events_listener)
+        .add_systems(Update, start_bonk.run_if(input_just_pressed(MouseButton::Middle)))
+        .add_systems(Update, end_bonk.run_if(input_pressed(MouseButton::Middle)));
 
         app.run();
 }
@@ -192,21 +194,172 @@ fn toggle_arrow_state(
     }
 }
 
-#[derive(Clone, Debug, Resource)] // could tie into player struct once assembled
+#[derive(Clone, Debug)] // could tie into player struct once assembled
+struct BonkMouseXY {
+    x: f32,
+    y: f32, 
+}
+
+impl BonkMouseXY {
+    fn new() -> Self {
+        let x: f32 = 0.0;
+        let y: f32 = 0.0;
+        BonkMouseXY {
+            x,
+            y,
+        }
+    }
+    
+    fn set(&mut self, x: f32, y: f32) {
+        self.x = x;
+        self.y = y;
+    }
+    
+    fn setx(&mut self, x: f32) {
+        self.x = x;
+    }
+    
+    fn sety(&mut self, y: f32) {
+        self.y = y;
+    }
+}
+
+#[derive(Clone, Debug, Resource)] // could tie into physics/player struct once assembled
 struct Bonk {
     direction: Vec3,
+    power: f32,
+    cursor_origin_position: BonkMouseXY,
+    cursor_origin_position_updated: bool,
+    cursor_bonk_position: BonkMouseXY,
+    cursor_bonk_position_updated: bool,
 }
 
 impl Bonk {
     fn new() -> Self {
         let direction: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+        let power: f32 = 0.0;
+        let cursor_origin_position: BonkMouseXY = BonkMouseXY::new();
+        let cursor_origin_position_updated: bool = false;
+        let cursor_bonk_position: BonkMouseXY = BonkMouseXY::new();
+        let cursor_bonk_position_updated: bool = false;
         Bonk {
             direction,
+            power,
+            cursor_origin_position,
+            cursor_origin_position_updated,
+            cursor_bonk_position,
+            cursor_bonk_position_updated,
         }
     }
 
     fn update_direction(&mut self, direction: &Vec3) {
         self.direction = *direction;
+    }
+
+    fn update_power(&mut self, power: f32) {
+        self.power = power;
+    }
+
+    fn update_cursor_origin_position(
+        &mut self, 
+        bonk_coords: BonkMouseXY
+    ) {
+        self.cursor_origin_position = bonk_coords;
+        self.cursor_origin_position_updated = true;
+    }
+
+    fn update_cursor_bonk_position(
+        &mut self, 
+        bonk_coords: BonkMouseXY
+    ) {
+        self.cursor_bonk_position = bonk_coords;
+        self.cursor_bonk_position_updated = true;
+    }
+
+    fn set_cursor_updated(&mut self) {
+        self.cursor_origin_position_updated = false;
+        self.cursor_bonk_position_updated = false;
+    }
+}
+
+
+
+
+fn start_bonk(
+    mut bonk: ResMut<Bonk>,
+    windows: Query<&Window>,
+) {
+    info!("Start bonk");
+    let mut cursor_xy: BonkMouseXY = BonkMouseXY::new();
+    let Some(position) = windows.single().cursor_position() else {
+        return;
+    };
+    cursor_xy.set(position.x, position.y);
+    bonk.update_cursor_origin_position(cursor_xy);
+    info!("bonk updated: pressed: {:?}", bonk.cursor_origin_position);
+}
+
+
+use bevy::window::WindowResolution;
+
+fn end_bonk(
+    mut bonk: ResMut<Bonk>,
+    windows: Query<&Window>,
+) {
+    info!("End bonk");
+    let mut cursor_xy: BonkMouseXY = BonkMouseXY::new();
+    let Some(position) = windows.single().cursor_position() else {
+        return;
+    };
+    let window_width: f32 = windows.single().width();
+    let window_height: f32 = windows.single().height();
+    cursor_xy.set(position.x, position.y);
+    bonk.update_cursor_bonk_position(cursor_xy);
+    info!("bonk updated: pressed: {:?}", bonk.cursor_bonk_position);
+
+    // find length of pixels from origin to release
+    let difference_x = bonk.cursor_origin_position.x - bonk.cursor_bonk_position.x;
+    let difference_y = bonk.cursor_origin_position.y - bonk.cursor_bonk_position.y;
+
+    // find percentage moved across screen to determine power of bonk in x & y
+    let dif_x_percentage = difference_x / window_width;
+    let dif_y_percentage = difference_y / window_height;
+
+    // Determin total bonk power normalized across both height and width 
+    let mut bonk_power = (dif_x_percentage + dif_y_percentage) / 2.0;
+    
+    // adjust bonk power to always deliver a positive value
+    if bonk_power < 0.0 {
+        bonk_power *= -1.0;
+    };
+
+    // shrinking the drag length to set power by doubling the value, 
+    // anything drag 50% of the screen or over will deliver 100%
+    if bonk_power >= 0.25 {
+        bonk_power = 1.0;
+    } else {
+        bonk_power *= 4.0;
+    };
+
+    info!("bonk_power: {:?}", bonk_power);
+    bonk.power = bonk_power;
+    bonk.set_cursor_updated();
+    info!(
+        "bool check: {} {}",
+        bonk.cursor_origin_position_updated,
+        bonk.cursor_bonk_position_updated,
+    );
+}
+
+fn bonk(
+    mut impulses: Query<&mut ExternalImpulse>,
+    bonk: Res<Bonk>,
+) {
+    for mut impulse in impulses.iter_mut() {
+        // Reset or set the impulse every frame
+        // impulse.impulse = Vec3::new(0.0, 0.0, 20.0);
+        impulse.impulse = -bonk.direction * (5.0 * bonk.power);
+        impulse.torque_impulse = Vec3::new(0.0, 0.0, 0.0);
     }
 }
 
@@ -239,13 +392,13 @@ fn bonk_gizmo (
         let point = intersection.position();
         for (name, transform) in scene_meshes.iter() {
             if name.as_str() == "ball" && transform.translation != Vec3::new(0.0, 0.0, 0.0) {
-                info!("Name: {:?}", &name);
-                info!("Transform: {:?}", &transform);
+                // info!("Name: {:?}", &name);
+                // info!("Transform: {:?}", &transform);
                 // Golf ball's position.
                 let ball_position = transform.translation;
 
                 // Calculate the direction from the ball to the intersection point.
-                let mut direction = (point - ball_position).normalize() * 10.0; // Scale to desired length.
+                let mut direction = (point - ball_position).normalize() * (12.0 * bonk.power); // Scale to desired length.
                 direction.y = 0.0;
                 bonk.update_direction(&direction);
                 // Draw an arrow from the ball in the direction toward the cursor.
@@ -255,7 +408,7 @@ fn bonk_gizmo (
                     Color::srgb(0.0, 1.0, 0.0),             // Color of the arrow
                 );
 
-                info!("Arrow drawn from ball at {:?} towards {:?}", ball_position, point);
+                // info!("Arrow drawn from ball at {:?} towards {:?}", ball_position, point);
             }
         }
     }
@@ -275,19 +428,6 @@ fn collision_events_listener(
         }
     }
 }
-
-fn bonk(
-    mut impulses: Query<&mut ExternalImpulse>,
-    bonk: Res<Bonk>,
-) {
-    for mut impulse in impulses.iter_mut() {
-        // Reset or set the impulse every frame
-        // impulse.impulse = Vec3::new(0.0, 0.0, 20.0);
-        impulse.impulse = -bonk.direction * 5.0;
-        impulse.torque_impulse = Vec3::new(0.0, 0.0, 0.0);
-    }
-}
-
 pub fn add_physics_query_and_update_scene(
     mut arrow_state: ResMut<State<ArrowState>>,
     mut next_arrow_state: ResMut<NextState<ArrowState>>,
